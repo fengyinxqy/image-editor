64499800ae65d0f55ed8eb108527aa5bf152a580
fengyinxqy <572752189@qq.com>
2023-05-25 13:33:56 +0800

    删除多余文件，配置干净目录

--- font-image-editor/src/App.vue
+++ font-image-editor/src/App.vue
@@ -1,10 +1,16 @@
+<!--
+ * @Author: fengyinxqy 572752189@qq.com
+ * @Date: 2023-05-25 12:26:14
+ * @LastEditors: fengyinxqy 572752189@qq.com
+ * @LastEditTime: 2023-05-25 13:31:56
+ * @FilePath: \font-image-editor\src\App.vue
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+-->
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    123
   </div>
 </template>
 
